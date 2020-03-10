c89df5b35a6c2f077c4de8090553ab314d8e715e
Arthur Oliveira <arthur2109.vieira@gmail.com>
2020-03-09 21:28:42 -0300

    More Unit tests

--- src/test/java/com/wipro/api/roles/detail/RoleDetailServiceTest.java
+++ src/test/java/com/wipro/api/roles/detail/RoleDetailServiceTest.java
@@ -14,6 +14,7 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
 
@@ -28,8 +29,17 @@ class RoleDetailServiceTest {
                 .given_repository_return_role()
                 .when_call_insert()
                 .then_role_not_null()
-                .when_call_detail()
-                .then_detail_was_called();
+                .when_call_detail();
+    }
+
+    @Test
+    public void test_insert_role_null_and_role_not_found_fail(){
+        new TestSpec()
+                .given_use_default_role_set_name("Admin")
+                .given_repository_return_role()
+                .when_call_insert()
+                .then_role_is_not_equals()
+                .when_call_detail_role_null();
     }
 
     static class TestSpec{
@@ -37,7 +47,7 @@ class RoleDetailServiceTest {
         @InjectMocks
         RoleCreateService roleCreateService;
 
-        @InjectMocks
+        @Mock
         RoleDetailService roleDetailService;
 
         @Mock
@@ -72,18 +82,20 @@ class RoleDetailServiceTest {
             return this;
         }
 
+        public TestSpec when_call_detail_role_null(){
+            roleDetailService.findById(null);
+            return this;
+        }
+
         public TestSpec then_role_not_null(){
             Assert.assertNotNull(roleInserted);
             return this;
         }
 
-        public TestSpec then_detail_was_called(){
-            verify(repository).findById(roleInserted.getId());
+        public TestSpec then_role_is_not_equals(){
+            assertThat(roleInserted.getName()).isNotEqualTo("Admn");
             return this;
         }
-
-
-
     }
 
 
--- src/test/java/com/wipro/api/users/detail/UsersDetailServiceTest.java
+++ src/test/java/com/wipro/api/users/detail/UsersDetailServiceTest.java
@@ -1,8 +1,7 @@
-package com.wipro.api.users.delete;
+package com.wipro.api.users.detail;
 
 import com.wipro.api.roles.detail.RoleDetailService;
 import com.wipro.api.users.create.UsersCreateSevice;
-import com.wipro.domain.role.Role;
 import com.wipro.domain.users.User;
 import com.wipro.domain.users.UserRepository;
 import org.junit.Assert;
@@ -14,70 +13,61 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.time.LocalDate;
 
+import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
-class UsersDeleteServiceTest {
+class UsersDetailServiceTest {
 
     @Test
-    public void test_insert_role_then_delete_success(){
+    public void test_insert_user_success(){
         new TestSpec()
                 .given_use_default_user_set_name()
                 .given_repository_return_user()
                 .when_call_insert()
                 .then_user_not_null()
-                .when_call_delete()
-                .then_delete_was_called();
+                .when_call_detail();
     }
 
     @Test
-    public void test_insert_user_then_delete_fail(){
+    public void test_insert_user_null_and_user_not_found_fail(){
         new TestSpec()
                 .given_use_default_user_set_name()
                 .given_repository_return_user()
                 .when_call_insert()
-                .then_user_not_null()
-                .when_call_delete()
-                .then_delete_was_called_with_id_not_found();
+                .then_user_is_not_equals()
+                .when_call_detail_user_null();
     }
 
-    private Role getRoleMock() {
-        Role role = mock(Role.class);
-        role.setName("Test Role");
-        role.setId(1L);
-        return role;
-    }
 
-    class TestSpec {
-
-        @Mock
-        UserRepository repository;
+     static class TestSpec {
 
         @InjectMocks
         UsersCreateSevice usersCreateSevice;
 
-        @InjectMocks
-        UsersDeleteService usersDeleteService;
-
         @Mock
         RoleDetailService roleDetailService;
+
+        @Mock
+        UsersDetailService usersDetailService;
+
+        @Mock
+        UserRepository repository;
 
         User user;
         User userInserted;
 
         TestSpec(){
-            MockitoAnnotations.initMocks(this);
-        }
+             MockitoAnnotations.initMocks(this);
+         }
 
         public TestSpec given_use_default_user_set_name(){
             user = new User();
+            user.setId(1L);
             user.setFirstName("First Name");
             user.setLastName("Last Name");
             user.setEmail("email@email.com");
@@ -85,17 +75,23 @@ class UsersDeleteServiceTest {
             return this;
         }
 
-        public TestSpec given_repository_return_user() {
+        public TestSpec given_repository_return_user(){
             BDDMockito.given(repository.save(any(User.class))).willReturn(user);
             return this;
         }
 
-        public TestSpec when_call_insert() throws IllegalArgumentException{
-
-            Role role = getRoleMock();
-
+        public TestSpec when_call_insert(){
             userInserted = usersCreateSevice.insert(user, 1L);
+            return this;
+        }
 
+        public TestSpec when_call_detail(){
+            usersDetailService.findById(userInserted.getId());
+            return this;
+        }
+
+        public TestSpec when_call_detail_user_null(){
+            usersDetailService.findById(null);
             return this;
         }
 
@@ -104,18 +100,8 @@ class UsersDeleteServiceTest {
             return this;
         }
 
-        public TestSpec when_call_delete() throws EmptyResultDataAccessException {
-            usersDeleteService.delete(userInserted.getId());
-            return this;
-        }
-
-        public TestSpec then_delete_was_called(){
-            verify(repository).deleteById(userInserted.getId());
-            return this;
-        }
-
-        public TestSpec then_delete_was_called_with_id_not_found(){
-            verify(repository).deleteById(null);
+        public TestSpec then_user_is_not_equals(){
+            assertThat(userInserted.getFirstName()).isNotEqualTo("Fist Name");
             return this;
         }
 
